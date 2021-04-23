fd271bd401e1fd46914e58952d5855407b773ac2
Marquise Piton <pitonmarquise@gmail.com>
2021-04-23 10:38:07 -0400

    Styled the items component

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,9 +5,9 @@ function Item({item}) {
   return (
     <div className="item">
       {item && <li className="list-group-item">
-        <h1>{item.name}</h1>
-        <p>{item.description}</p>
-        <p>{item.price}</p>
+        <h1 className='stuff'>{item.name}</h1>
+        <p className='stuff'>{item.description}</p>
+        <p className="price">{item.price}</p>
         <Link to={`product/${item.id}`}>
           <button className="item-button">More Info</button>
         </Link>
